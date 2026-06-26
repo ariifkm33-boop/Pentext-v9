cf957fb2f50a66b9fe81658a9179f8b7b3e7dff8
ariifkm33-boop <ariifkm33@gmail.com>
2026-06-26 08:37:23 +0600

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,6 @@ CHANNEL_USERNAME = os.environ.get('CHANNEL_USERNAME', '@saniedit9')
 if not CHANNEL_USERNAME.startswith('@'):
     CHANNEL_USERNAME = '@' + CHANNEL_USERNAME
 
-REQUIRED_REFS = 2
-FREE_LINKS = 2
-MAX_LINKS_PER_HOUR = 5
-
 DB_PATH = '/data/database.sqlite'
 BASE_URL = os.environ.get('BASE_URL', 'http://localhost:8000').rstrip('/')
 PORT = int(os.environ.get('PORT', '8000'))
